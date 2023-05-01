119433dc208a62f975a5dc3ce2bb42c00d3b387f
krishna479 <118340986+krishna479@users.noreply.github.com>
2023-05-01 23:42:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const gridItems = document.querySelectorAll('.grid-item');
+const blockIdInput = document.getElementById('block_id');
+const colorInput = document.getElementById('color_id');
+const changeButton = document.getElementById('change_button');
+const resetButton = document.getElementById('reset_button');
+
+changeButton.addEventListener('click', () => {
+  const blockId = blockIdInput.value;
+  const color = colorInput.value;
+  const block = document.getElementById(blockId);
+  
+  if (block) {
+    gridItems.forEach((item) => {
+      item.style.backgroundColor = 'transparent';
+    });
+    
+    block.style.backgroundColor = color;
+  }
+});
+
+resetButton.addEventListener('click', () => {
+  gridItems.forEach((item) => {
+    item.style.backgroundColor = 'transparent';
+  });
+});
